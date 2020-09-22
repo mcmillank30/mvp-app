a7ab8fe70fdceb944cd27867fa7d79d355527fd8
mcmillak30 <mcmillank30.github.io>
2020-09-22 12:28:21 -0500

    front-end is connected to back-end!!

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 require('dotenv').config()// importing environmental variable
 const express = require('express'); //importing express
 const { PORT } = process.env; //importing PORT from .env file
+const path = require('path');
 
 const app = express();
 // app.listen(8080, () => {
 //   console.log('listening on 8080')
 // })
 
-
-
+//app.use('/', express.static('./client/dist'))
+app.use('/', express.static(path.resolve(__dirname, '../client', 'dist')))//line 12 and 4, do the same thing as line 11. Connects my front end to my backend.
 
 
 app.listen(PORT, () => {
