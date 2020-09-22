1c9d83582d50e8cf1726f1a90a3e0dbaf3b24743
mcmillak30 <mcmillank30.github.io>
2020-09-22 17:57:56 -0500

    setting up get weather method

--- client/src/components/Forms.jsx
+++ client/src/components/Forms.jsx
@@ -7,8 +7,13 @@ class Forms extends React.Component {
   render() {
 
     return (
-
-      <div> </div>
+      <div>
+        <form>
+          <input type="text" name="city" placeholder= "Whats the city?"/>
+          <input type="text" name="country" placeholder= "Whats the country"/>
+          <button>Click Me</button>
+        </form>
+      </div>
     )
   }
 }
--- client/src/components/Titles.jsx
+++ client/src/components/Titles.jsx
@@ -8,7 +8,10 @@ class Titles extends React.Component {
 
     return (
 
-      <div> </div>
+      <div>
+        <h1>What's the Weather</h1>
+        <p>Never get caught out in the rain, What's The Weather App; will help you stay prepared, whatever the weather!</p>
+      </div>
     )
   }
 }
