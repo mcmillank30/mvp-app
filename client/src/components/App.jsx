b3fabd83dc7a22d3f864be531eee2c0879cc9136
mcmillak30 <mcmillank30.github.io>
2020-09-22 23:12:39 -0500

    working of search functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,28 +3,47 @@ import Forms from "./Forms.jsx";
 import Titles from "./Titles.jsx";
 import Weather from "./Weather.jsx";
 
+ 
 
 
 
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
+    this.state = {
 
-  // getTheWeather = async() => {
-  //   // const call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`)
-  //   // const data = await call.json()
+    }
+    // this.getTheWeather = this.getTheWeather.bind(this);
   }
 
- 
+  // getTheWeather = async () {
+  //   const call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`)
+  //   const data = call.json()
+  //   console.log(data)
+  // }
+
+  componentDidMount(e) {
+    e.preventDefault();
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`)
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }
+
+  render() {
+
     return (
 
       <div>
         <Titles />
-        <Forms />
+        <Forms getTheWeather={this.componentDidMount}/>
         <Weather />
       </div>
     )
-  
+  }
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const app = express();
 // })
 
 //app.use('/', express.static('./client/dist'))
-app.use('/', express.static(path.resolve(__dirname, '../client', 'dist')))//line 12 and 4, do the same thing as line 11. Connects my front end to my backend.
+app.use('/', express.static(path.resolve(__dirname, '../client', 'dist')))//line 12 and 4, do the same thing as line 11. Connects my front end to my backend.(refer to node.js docs)
+
+app.get('/', (req, res) => {})
+app.post('/', (req, res) => {})
+app.put('/', (req, res) => {})
+app.delete('/', (req, res) => {})
+
 
 
 app.listen(PORT, () => {
