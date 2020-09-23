31045c14e830cf8e243e0632e44e0de9ae767fb8
mcmillak30 <mcmillank30.github.io>
2020-09-23 12:45:20 -0500

    app functioning properly

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-class Weather extends React.Component {
-
-  render() {
+const Weather = ({ city, temperature, humidity, error, description, country }) => {
 
     return (
 
-      <div> </div>
+      <div>
+        {city && country && <p>Location: {city}, {country}</p>}
+        {temperature && <p>temperature: {temperature}</p>}
+        {humidity && <p>Humidity: {humidity}</p>}
+        {description && <p>Description: {description}</p>}
+        {error && <p>{error}</p>}
+
+      </div>
     )
-  }
+  
 }
 
 export default Weather;
