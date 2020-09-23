d584319db4d3a0874515eea930a175ff2041f0dc
mcmillak30 <mcmillank30.github.io>
2020-09-22 23:12:39 -0500

    working of search functionality

--- client/src/components/Forms.jsx
+++ client/src/components/Forms.jsx
@@ -8,9 +8,9 @@ class Forms extends React.Component {
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.props.getTheWeather}>
           <input type="text" name="city" placeholder= "Whats the city?"/>
-          <input type="text" name="country" placeholder= "Whats the country"/>
+          <input type="text" name="country" placeholder= "Whats the country?"/>
           <button>Click Me</button>
         </form>
       </div>
